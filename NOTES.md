bd82341c1b56077be85a64cfe526998b6db7c2d3
Gary Chambers <gary@repro.dev>
2021-10-11 13:51:46 +0100

    Add resource proxy (CORS bypass)

--- NOTES.md
+++ NOTES.md
@@ -2,3 +2,4 @@
 
 - 3rd-party iframes: https://www.lmax.com
 - Canvas: https://trade.kraken.com
+- Checkbox state: https://www.bryanbraun.com/2021/09/21/i-keep-making-things-out-of-checkboxes/
---
+++ modules/resource-proxy/src/index.ts
@@ -0,0 +1,23 @@
+import http from 'http'
+import httpProxy from 'http-proxy'
+
+const proxy = httpProxy.createProxyServer()
+
+const server = http.createServer((req, res) => {
+  const headers = req.headers
+  const target = headers['x-proxy-target']
+
+  if (target && typeof target === 'string') {
+    proxy.web(req, res, {
+      target,
+      changeOrigin: true,
+    })
+
+    return
+  }
+
+  res.writeHead(400, 'Bad request: missing proxy target')
+  res.end()
+})
+
+server.listen(+(process.env.PORT || 8080))
