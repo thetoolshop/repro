aba26724518c1bfd5a0057753b0ded7c16dc6b29
Gary Chambers <gary@repro.dev>
2022-01-10 17:04:07 +0000

    Change browser action to global toggle

--- src/apps/chrome-extension/background.ts
+++ src/apps/chrome-extension/background.ts
@@ -2,93 +2,77 @@ const StorageKeys = {
   ENABLED: 'enabled',
 }
 
-chrome.runtime.onStartup.addListener(() => {
-  // Treat local storage as session storage
-  // TODO: support session and persistent storage
-  chrome.storage.local.clear()
-})
-
 chrome.runtime.onConnect.addListener(() => {
   ;(async function () {
-    const tab = await getActiveTab()
+    const enabled = await isEnabled()
 
-    if (tab.id && (await isEnabled(tab.id))) {
-      enable()
+    if (enabled) {
+      enableAll()
     }
   })()
 })
 
 chrome.action.onClicked.addListener(() => {
-  toggle()
+  toggleAll()
 })
 
-async function enable() {
-  const tab = await getActiveTab()
-
-  if (tab.id) {
-    showActionBadge(tab.id)
-    chrome.tabs.sendMessage(tab.id, { action: 'enable' })
-    chrome.storage.local.set({
-      [`${StorageKeys.ENABLED}:${tab.id}`]: true,
-    })
-  }
-}
-
-async function disable() {
-  const tab = await getActiveTab()
-
-  if (tab.id) {
-    hideActionBadge(tab.id)
-    chrome.tabs.sendMessage(tab.id, { action: 'disable' })
-    chrome.storage.local.set({
-      [`${StorageKeys.ENABLED}:${tab.id}`]: false,
-    })
-  }
-}
-
-function isEnabled(tabId: number): Promise<boolean> {
-  return new Promise(resolve => {
-    chrome.storage.local.get([`${StorageKeys.ENABLED}:${tabId}`], result => {
-      resolve(result[`${StorageKeys.ENABLED}:${tabId}`] || false)
-    })
-  })
-}
-
-async function toggle() {
-  const tab = await getActiveTab()
-
-  if (tab.id && (await isEnabled(tab.id))) {
-    disable()
-  } else {
-    enable()
-  }
-}
-
-async function getActiveTab() {
-  return new Promise<chrome.tabs.Tab>((resolve, reject) => {
-    chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
-      const activeTab = tabs[0]
-
-      if (activeTab) {
-        resolve(activeTab)
-      } else {
-        reject()
+async function enableAll() {
+  chrome.tabs.query({}, tabs => {
+    for (const tab of tabs) {
+      if (tab.id) {
+        showActionBadge()
+        chrome.tabs.sendMessage(tab.id, { action: 'enable' })
+        chrome.storage.local.set({
+          [StorageKeys.ENABLED]: true,
+        })
       }
+    }
+  })
+}
+
+async function disableAll() {
+  chrome.tabs.query({}, tabs => {
+    for (const tab of tabs) {
+      if (tab.id) {
+        hideActionBadge()
+        chrome.tabs.sendMessage(tab.id, { action: 'disable' })
+        chrome.storage.local.set({
+          [StorageKeys.ENABLED]: false,
+        })
+      }
+    }
+  })
+}
+
+function isEnabled(): Promise<boolean> {
+  return new Promise(resolve => {
+    chrome.storage.local.get([StorageKeys.ENABLED], result => {
+      resolve(result[StorageKeys.ENABLED] || false)
     })
   })
 }
 
-function showActionBadge(tabId: number) {
+async function toggleAll() {
+  const enabled = await isEnabled()
+
+  if (enabled) {
+    disableAll()
+  } else {
+    enableAll()
+  }
+}
+
+function showActionBadge() {
   return Promise.all([
-    chrome.action.setBadgeText({ tabId, text: 'on' }),
-    chrome.action.setBadgeBackgroundColor({ tabId, color: [128, 128, 128, 1] }),
+    chrome.action.setBadgeText({ text: 'on' }),
+    chrome.action.setBadgeBackgroundColor({ color: [128, 128, 128, 1] }),
   ])
 }
 
-function hideActionBadge(tabId: number) {
+function hideActionBadge() {
   return Promise.all([
-    chrome.action.setBadgeText({ tabId, text: '' }),
-    chrome.action.setBadgeBackgroundColor({ tabId, color: [0, 0, 0, 0] }),
+    chrome.action.setBadgeText({ text: '' }),
+    chrome.action.setBadgeBackgroundColor({ color: [0, 0, 0, 0] }),
   ])
 }
 
