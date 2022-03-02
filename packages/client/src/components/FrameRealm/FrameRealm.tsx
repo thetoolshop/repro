c0b5ffc5311f2845819384663df8dbc2a513cdce
Gary Chambers <gary@repro.dev>
2022-03-02 17:17:41 +0000

    Used TrustedHTML (if supported) to write doctype in FrameRealm

--- packages/client/src/components/FrameRealm/FrameRealm.tsx
+++ packages/client/src/components/FrameRealm/FrameRealm.tsx
@@ -5,6 +5,18 @@ import mergeRefs from 'react-merge-refs'
 
 type Props = React.HTMLProps<HTMLIFrameElement>
 
+// Bypass trusted-types CSP when writing doctype
+const passthroughHTMLPolicy =
+  window.trustedTypes && window.trustedTypes.createPolicy
+    ? window.trustedTypes.createPolicy('passthrough-html', {
+        createHTML: html => html,
+      })
+    : null
+
+function createTrustedHTMLIfSupported(html: string) {
+  return passthroughHTMLPolicy ? passthroughHTMLPolicy.createHTML(html) : html
+}
+
 export const FrameRealm = React.forwardRef<HTMLIFrameElement, Props>(
   ({ children, ...props }, outerRef) => {
     const innerRef = useRef() as MutableRefObject<HTMLIFrameElement>
@@ -17,7 +29,10 @@ export const FrameRealm = React.forwardRef<HTMLIFrameElement, Props>(
 
         if (doc) {
           doc.open()
-          doc.write('<!doctype html>')
+          // The type for Document.write sink in lib.dom does not currently
+          // support passing TrustedHTML object. Cast as string to keep
+          // the type-checker happy.
+          doc.write(createTrustedHTMLIfSupported('<!doctype html>') as string)
           doc.close()
 
           doc.domain = document.domain
