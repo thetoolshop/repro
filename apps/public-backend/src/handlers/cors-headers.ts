e3f6abe71326fb96a3ef79d2f90a4d538f8e528c
Gary Chambers <gary@repro.dev>
2022-09-23 04:44:46 +0100

    Drop obselete public-backend service

--- apps/public-backend/src/handlers/cors-headers.ts
+++
@@ -1,5 +0,0 @@
-export const corsHeaders = {
-  'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Headers': 'Accept, Content-Type, Content-Length',
-  'Access-Control-Expose-Headers': '*',
-}
--- apps/public-backend/src/handlers/preflight.ts
+++
@@ -1,7 +0,0 @@
-import { corsHeaders } from './cors-headers'
-
-export function handlePreflight(_request: Request) {
-  return new Response(null, {
-    headers: corsHeaders,
-  })
-}
