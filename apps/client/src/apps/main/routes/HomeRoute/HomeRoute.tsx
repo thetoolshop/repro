5a0b70e4e89fe937d4c37a6f667e59af504790d6
Gary Chambers <gary@repro.dev>
2023-02-03 12:40:30 +0000

    Update styling for recording list view

---
+++ apps/client/src/apps/main/routes/HomeRoute/HomeRoute.tsx
@@ -0,0 +1,34 @@
+import { RecordingMetadata } from '@repro/domain'
+import { fork, map } from 'fluture'
+import { Col } from 'jsxstyle'
+import React, { useEffect, useState } from 'react'
+import { Card } from '~/components/Card'
+import { useApiClient } from '~/libs/api'
+import { logger } from '~/libs/logger'
+import { RecordingTile } from './RecordingTile'
+
+function sortByCreated(recordings: Array<RecordingMetadata>) {
+  return recordings.sort((a, b) => (b.createdAt > a.createdAt ? 1 : -1))
+}
+
+export const HomeRoute: React.FC = () => {
+  const [recordings, setRecordings] = useState<Array<RecordingMetadata>>([])
+  const apiClient = useApiClient()
+
+  useEffect(() => {
+    return apiClient.recording
+      .getAllRecordings()
+      .pipe(map(sortByCreated))
+      .pipe(fork(logger.error)(setRecordings))
+  }, [apiClient, setRecordings])
+
+  return (
+    <Card>
+      <Col gap={10}>
+        {recordings.map(recording => (
+          <RecordingTile key={recording.id} recording={recording} />
+        ))}
+      </Col>
+    </Card>
+  )
+}
---
+++ apps/client/src/apps/main/routes/HomeRoute/RecordingTile.tsx
@@ -0,0 +1,91 @@
+import {
+  RecordingMetadata,
+  RecordingMode,
+  SourceEventType,
+} from '@repro/domain'
+import { Block, Col, Grid, InlineRow, Row } from 'jsxstyle'
+import { Camera as CameraIcon, Video as VideoIcon } from 'lucide-react'
+import React, { Fragment, useEffect, useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { colors } from '~/config/theme'
+import { formatDate, formatTime } from '~/utils/date'
+
+interface Props {
+  recording: RecordingMetadata
+}
+
+export const RecordingTile: React.FC<Props> = ({ recording }) => {
+  const navigate = useNavigate()
+
+  function onClick() {
+    navigate(`/recordings/${recording.id}`)
+  }
+
+  return (
+    <Col
+      key={recording.id}
+      padding={15}
+      gap={10}
+      fontSize={13}
+      color={colors.slate['800']}
+      backgroundColor={colors.slate['100']}
+      borderColor="transparent"
+      borderWidth={1}
+      borderStyle="solid"
+      borderRadius={4}
+      cursor="pointer"
+      hoverBackgroundColor={colors.white}
+      hoverBorderColor={colors.blue['500']}
+      hoverColor={colors.blue['700']}
+      hoverBoxShadow={`0 4px 8px ${colors.slate['200']}`}
+      transition="all linear 100ms"
+      props={{ onClick }}
+    >
+      <Row alignItems="center" gap={10}>
+        <InlineRow
+          alignItems="center"
+          gap={10}
+          height={24}
+          paddingH={8}
+          backgroundColor={colors.blue['100']}
+          color={colors.blue['700']}
+          borderRadius={4}
+        >
+          {(recording.mode === RecordingMode.Live ||
+            recording.mode === RecordingMode.Replay) && (
+            <Fragment>
+              <VideoIcon size={16} />
+              <Block>{formatTime(recording.duration, 'seconds')}</Block>
+            </Fragment>
+          )}
+
+          {recording.mode === RecordingMode.Snapshot && (
+            <Fragment>
+              <CameraIcon size={16} />
+              <Block>Screenshot</Block>
+            </Fragment>
+          )}
+        </InlineRow>
+
+        <Block
+          component={Link}
+          color="inherit"
+          fontSize={16}
+          textDecoration="none"
+          lineHeight={1.5}
+          props={{ to: `/recordings/${recording.id}` }}
+        >
+          {recording.title}
+        </Block>
+      </Row>
+
+      <Row alignItems="center" gap={10} fontSize={13} lineHeight={1.5}>
+        <Block color={colors.slate['700']}>{recording.authorName}</Block>
+
+        <Block color={colors.slate['700']}>
+          {formatDate(recording.createdAt)}
+        </Block>
+      </Row>
+    </Col>
+  )
+}
