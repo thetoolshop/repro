0b32c6ad56001a70896fbb8fbc1f6d84238d6a99
Gary Chambers <gary@repro.dev>
2023-08-21 16:48:14 +0100

    Fix tdl package exports (#114)

--- packages/tdl/src/index.ts
+++ packages/tdl/src/index.ts
@@ -9,6 +9,8 @@ import {
 
 export * as z from 'zod'
 export * from './lib/descriptors'
+// These probably don't belong as exports from this module
+export { approxByteLength, copy } from './lib/utils'
 export * from './lib/view'
 
 // Temporary exports for backwards compatibility
