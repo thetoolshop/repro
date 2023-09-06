3bb9aaf7fcfb6c91669624e2b6521fa871bd4ef9
Gary Chambers <gary@repro.dev>
2023-09-06 15:00:02 +0000

    Rename Repro client app to workspace

--- scripts/tmux.sh
+++ scripts/tmux.sh
@@ -8,9 +8,12 @@ SESSIONNAME=repro
 tmux has-session -t $SESSIONNAME &> /dev/null
 
 if [ $? != 0 ]; then
-  tmux new-session -s $SESSIONNAME -n client -c $BASE_PATH/apps/repro/client -d
-  tmux split-window -c $BASE_PATH/apps/repro/client -h "npm run watch:capture; bash -i"
-  tmux split-window -c $BASE_PATH/apps/repro/client -v "npm run serve:main; bash -i"
+  tmux new-session -s $SESSIONNAME -n workspace -c $BASE_PATH/apps/repro/workspace -d
+  tmux split-window -c $BASE_PATH/apps/repro/workspace -h "npm run serve; bash -i"
+  tmux select-pane -t 0
+
+  tmux new-window -n capture -c $BASE_PATH/apps/repro/capture
+  tmux split-window -c $BASE_PATH/apps/repro/capture -h "npm run watch; bash -i"
   tmux select-pane -t 0
 
   tmux new-window -n api-server -c $BASE_PATH/apps/repro/api-server
@@ -18,7 +21,7 @@ if [ $? != 0 ]; then
   tmux split-window -c $BASE_PATH/apps/repro/api-server -v "npm run dev-resources:start; bash -i"
   tmux select-pane -t 0
 
-  tmux select-window -t client
+  tmux select-window -t workspace
 fi
 
 tmux attach -t $SESSIONNAME
