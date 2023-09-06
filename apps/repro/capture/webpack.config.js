719860a3877168c5ed5e966a34a1b3af752462f9
Gary Chambers <gary@repro.dev>
2023-09-06 16:08:45 +0100

    Split Repro client apps (#136)
    * Move Repro apps to apps/repro subdirectory
    
    * Change build output paths in CI actions
    
    * Fix Dockerfile paths
    
    * Update dockerfiles
    
    * Split capture extension into standalone app
    
    * Rename Repro client app to workspace

---
+++ apps/repro/capture/webpack.config.js
@@ -0,0 +1,60 @@
+const DotenvPlugin = require('dotenv-webpack')
+const { EsbuildPlugin } = require('esbuild-loader')
+const path = require('path')
+
+module.exports = {
+  mode: process.env.BUILD_ENV === 'production' ? 'production' : 'development',
+
+  entry: {
+    background: path.resolve(__dirname, 'src/extension/background.ts'),
+    content: path.resolve(__dirname, 'src/extension/content.ts'),
+    capture: path.resolve(__dirname, 'src/index.tsx'),
+  },
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+  },
+
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
+    alias: {
+      '~': path.resolve(__dirname, 'src'),
+    },
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'esbuild-loader',
+        options: {
+          loader: 'tsx',
+          minify: true,
+          target: 'es2015',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+
+  optimization: {
+    minimizer: [
+      new EsbuildPlugin({
+        target: 'es2015',
+      }),
+    ],
+  },
+
+  plugins: [
+    new DotenvPlugin({
+      path: process.env.ENV_FILE || '.env',
+      safe: true,
+      systemvars: true,
+    }),
+  ],
+
+  devtool: process.env.BUILD_ENV === 'development' ? 'source-map' : false,
+}
--- scripts/tmux.sh
+++ scripts/tmux.sh
@@ -8,17 +8,20 @@ SESSIONNAME=repro
 tmux has-session -t $SESSIONNAME &> /dev/null
 
 if [ $? != 0 ]; then
-  tmux new-session -s $SESSIONNAME -n client -c $BASE_PATH/apps/client -d
-  tmux split-window -c $BASE_PATH/apps/client -h "npm run watch:capture; bash -i"
-  tmux split-window -c $BASE_PATH/apps/client -v "npm run serve:main; bash -i"
+  tmux new-session -s $SESSIONNAME -n workspace -c $BASE_PATH/apps/repro/workspace -d
+  tmux split-window -c $BASE_PATH/apps/repro/workspace -h "npm run serve; bash -i"
   tmux select-pane -t 0
 
-  tmux new-window -n api-server -c $BASE_PATH/apps/api-server
-  tmux split-window -c $BASE_PATH/apps/api-server -h "npm run watch; bash -i"
-  tmux split-window -c $BASE_PATH/apps/api-server -v "npm run dev-resources:start; bash -i"
+  tmux new-window -n capture -c $BASE_PATH/apps/repro/capture
+  tmux split-window -c $BASE_PATH/apps/repro/capture -h "npm run watch; bash -i"
   tmux select-pane -t 0
 
-  tmux select-window -t client
+  tmux new-window -n api-server -c $BASE_PATH/apps/repro/api-server
+  tmux split-window -c $BASE_PATH/apps/repro/api-server -h "npm run watch; bash -i"
+  tmux split-window -c $BASE_PATH/apps/repro/api-server -v "npm run dev-resources:start; bash -i"
+  tmux select-pane -t 0
+
+  tmux select-window -t workspace
 fi
 
 tmux attach -t $SESSIONNAME
