b210ef9056467473f32703656327a7b72c6cf0be
Gary Chambers <gary@repro.dev>
2024-07-09 19:57:51 +0100

    Add support for standalone dev-toolbar build

--- apps/dev-toolbar/src/index.tsx
+++ apps/dev-toolbar/src/index.tsx
@@ -1,142 +1,50 @@
 import { Stats, Trace } from '@repro/diagnostics'
-import { createPTPAgent, MessagingProvider } from '@repro/messaging'
-import {
-  createRecordingStream,
-  RecordingStreamProvider,
-} from '@repro/recording'
-import { applyResetStyles } from '@repro/theme'
+import { Agent, createLoopbackAgent, createPTPAgent } from '@repro/messaging'
 import { resolve } from 'fluture'
-import { cache as styleCache } from 'jsxstyle'
-import React from 'react'
-import { createRoot, Root } from 'react-dom/client'
-import { TOOLBAR_ROOT_ID } from './constants'
-import { StateProvider } from './context'
-import { Controller } from './Controller'
-import { createState } from './createState'
+import { attach, detach, usingAgent } from './ToolshopDevToolbar'
+
+declare global {
+  const __BUILD_VERSION__: string | undefined
+}
+
+const standalone = process.env.MODE === 'standalone'
+
+console.log('Repro build version:', __BUILD_VERSION__)
+console.log('Repro build mode:', process.env.MODE ?? '(default: extension)')
 
 if (process.env.NODE_ENV === 'development') {
   Stats.enable()
   Trace.enable()
 }
 
-const NODE_NAME = 'toolshop-dev-toolbar'
+let agent: Agent
 
-// jsxstyle prevents multiple invocations of `cache.injectOptions`,
-// so we cannot register a new style root per custom element.
-// We must keep track of the active style root in global context instead.
-// NB: if we need to support multiple instances, this could hold
-// WeakMap<ReproDevTools, HTMLStyleElement>
-interface Refs {
-  activeStyleRoot: HTMLStyleElement | null
+switch (process.env.MODE) {
+  case 'standalone':
+    agent = createLoopbackAgent()
+    break
+
+  case 'extension':
+  default:
+    agent = createPTPAgent()
+    break
 }
 
-const refs: Refs = {
-  activeStyleRoot: null,
-}
-
-const _initialInjectOptions = styleCache.injectOptions
-
-// Should PTP agent replace Broadcast agent?
-const agent = createPTPAgent()
-
-class ToolshopDevToolbar extends HTMLElement {
-  private renderRoot: Root
-  private state = createState()
-
-  constructor() {
-    super()
-
-    const shadowRoot = this.attachShadow({ mode: 'closed' })
-
-    const rootElem = document.createElement('div')
-    rootElem.id = TOOLBAR_ROOT_ID
-    this.renderRoot = createRoot(rootElem)
-
-    const styleRoot = document.createElement('style')
-    refs.activeStyleRoot = styleRoot
-
-    shadowRoot.appendChild(styleRoot)
-    shadowRoot.appendChild(rootElem)
-
-    styleCache.reset()
-    styleCache.injectOptions = _initialInjectOptions
-    // TODO: build and bundle css for prod
-    styleCache.injectOptions({
-      onInsertRule(rule) {
-        if (refs.activeStyleRoot) {
-          const sheet = refs.activeStyleRoot.sheet
-
-          if (sheet) {
-            sheet.insertRule(rule, sheet.cssRules.length)
-          }
-        }
-      },
-    })
-  }
-
-  public connectedCallback() {
-    const ignoredSelectors = [NODE_NAME]
-    const ignoredNodes: Array<Node> = []
-
-    if (this.shadowRoot) {
-      ignoredNodes.push(this.shadowRoot)
-    }
-
-    if (document.currentScript) {
-      ignoredNodes.push(document.currentScript)
-    }
-
-    const stream = createRecordingStream(document, {
-      types: new Set([
-        'dom',
-        'interaction',
-        'network',
-        'console',
-        'performance',
-      ]),
-      ignoredNodes,
-      ignoredSelectors,
-    })
-
-    if (refs.activeStyleRoot) {
-      applyResetStyles(`#${TOOLBAR_ROOT_ID}`, refs.activeStyleRoot)
-    }
-
-    this.renderRoot.render(
-      <RecordingStreamProvider stream={stream}>
-        <StateProvider state={this.state}>
-          <MessagingProvider agent={agent}>
-            <Controller />
-          </MessagingProvider>
-        </StateProvider>
-      </RecordingStreamProvider>
-    )
-  }
-
-  public disconnectedCallback() {
-    this.renderRoot.unmount()
-  }
-}
+usingAgent(agent)
 
 agent.subscribeToIntent('enable', () => {
-  if (!window.customElements.get(NODE_NAME)) {
-    window.customElements.define(NODE_NAME, ToolshopDevToolbar)
-  }
-
-  if (!document.querySelector(NODE_NAME)) {
-    const root = new ToolshopDevToolbar()
-    document.body.appendChild(root)
-  }
-
+  attach()
   return resolve<void>(undefined)
 })
 
 agent.subscribeToIntent('disable', () => {
-  const root = document.querySelector(NODE_NAME)
-
-  if (root) {
-    root.remove()
-  }
-
+  detach()
   return resolve<void>(undefined)
 })
+
+// Auto-start in standalone build
+if (standalone) {
+  agent.raiseIntent({
+    type: 'enable',
+  })
+}
--- apps/dev-toolbar/webpack.config.js
+++ apps/dev-toolbar/webpack.config.js
@@ -1,21 +1,31 @@
 const DotenvPlugin = require('dotenv-webpack')
 const { EsbuildPlugin } = require('esbuild-loader')
 const path = require('path')
+const webpack = require('webpack')
 
 const env = process.env.NODE_ENV || 'development'
+const mode = process.env.MODE || 'extension'
 
 require('dotenv').config({
   path: path.resolve(__dirname, `.env.${env}`),
 })
 
+const commitHash = require('child_process')
+  .execSync('git rev-parse --short HEAD')
+  .toString()
+  .trim()
+
 module.exports = {
   mode: process.env.BUILD_ENV === 'production' ? 'production' : 'development',
 
-  entry: {
-    background: path.resolve(__dirname, 'src/extension/background.ts'),
-    content: path.resolve(__dirname, 'src/extension/content.ts'),
-    page: path.resolve(__dirname, 'src/index.tsx'),
-  },
+  entry:
+    mode === 'standalone'
+      ? { 'toolbar-standalone': path.resolve(__dirname, 'src/index.tsx') }
+      : {
+          background: path.resolve(__dirname, 'src/extension/background.ts'),
+          content: path.resolve(__dirname, 'src/extension/content.ts'),
+          page: path.resolve(__dirname, 'src/index.tsx'),
+        },
 
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -54,6 +64,10 @@ module.exports = {
   },
 
   plugins: [
+    new webpack.DefinePlugin({
+      __BUILD_VERSION__: JSON.stringify(commitHash),
+    }),
+
     new DotenvPlugin({
       path: path.resolve(__dirname, `./.env.${process.env.BUILD_ENV}`),
       safe: true,
