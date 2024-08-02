c9365cebd8525428bc14adb13a2dd57459af8a2c
Gary Chambers <gary@repro.dev>
2024-08-02 22:03:17 +0100

    Production deployment config

--- infra/appsets/production.config.js
+++ infra/appsets/production.config.js
@@ -1,3 +1,14 @@
 module.exports = {
   apps: [require("../apps/api-server").prod],
+
+  deploy: {
+    production: {
+      host: ["production"],
+      ref: "origin/main",
+      repo: "git@github.com:thetoolshop/repro.git",
+      path: "/home/me/Projects/thetoolshop/repro",
+      "post-deploy":
+        "pnpm install && pnpm pm2 startOrRestart infra/appsets/production.config.js",
+    },
+  },
 };
