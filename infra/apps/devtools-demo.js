8472f7c1a035c7593bbce2b65652062fdc689ad6
Gary Chambers <gary@repro.dev>
2024-08-05 16:48:41 +0100

    Fix devtools SPA root

--- infra/apps/devtools-demo.js
+++ infra/apps/devtools-demo.js
@@ -17,10 +17,10 @@ module.exports = {
     ...baseConfig,
     script: "serve",
     env: {
-      PM2_SERVE_PATH: "dist/",
+      PM2_SERVE_PATH: "dist",
       PM2_SERVE_PORT: 8080,
       PM2_SERVE_SPA: true,
-      PM2_SERVE_HOMEPAGE: "./index.html",
+      PM2_SERVE_HOMEPAGE: "/index.html",
     },
   },
 };
