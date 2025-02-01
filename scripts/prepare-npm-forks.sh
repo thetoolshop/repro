6900d1a24db45f70f0f1c638ab8ed8d726ed5f0b
Gary Chambers <gary@repro.dev>
2025-02-01 14:02:57 +0000

    Add caching for npm-forks build output (#163)
    * feat: add caching for npm-forks build output
    
    * Update environment variables from NPM_FORKS_CACHE_HIT to CI_NPM_FORKS_CACHE_HIT

--- scripts/prepare-npm-forks.sh
+++ scripts/prepare-npm-forks.sh
@@ -4,6 +4,12 @@ set -u
 
 BASE_PATH=$(realpath "$(dirname $BASH_SOURCE[0])/..")
 
+# Skip build if we have a cache hit in CI
+if [ "${CI_NPM_FORKS_CACHE_HIT:-}" = "true" ]; then
+  echo "Skipping prepare-npm-forks due to cache hit"
+  exit 0
+fi
+
 echo $BASE_PATH
 
 for d in "$BASE_PATH/npm-forks/*/"; do
