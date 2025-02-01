00997a1b9f154f79bd9f7c78a3ed3ab2c9613b59
Gary Chambers <gary@repro.dev>
2025-02-01 14:00:38 +0000

    Update environment variables from NPM_FORKS_CACHE_HIT to CI_NPM_FORKS_CACHE_HIT

--- scripts/prepare-npm-forks.sh
+++ scripts/prepare-npm-forks.sh
@@ -5,7 +5,7 @@ set -u
 BASE_PATH=$(realpath "$(dirname $BASH_SOURCE[0])/..")
 
 # Skip build if we have a cache hit in CI
-if [ "${NPM_FORKS_CACHE_HIT:-}" = "true" ]; then
+if [ "${CI_NPM_FORKS_CACHE_HIT:-}" = "true" ]; then
   echo "Skipping prepare-npm-forks due to cache hit"
   exit 0
 fi
